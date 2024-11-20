c05e91e325fbb4ab5d25436ba2683fe05954330d
Ar3cibo <knaito@protonmail.com>
2024-11-20 13:58:18 +0900

    [front] リストが表示されるように

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,30 @@ function App() {
     const [votes, setVotes] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    const votes_test = [
+        {
+            id: 1,
+            vote_title: '何県に行きたいですか',
+            added_user_id: 1,
+            is_Closed: false,
+            updated: 2024 - 11 - 20,
+        },
+        {
+            id: 2,
+            vote_title: 'どこの国に行きたいですか',
+            added_user_id: 1,
+            is_Closed: false,
+            updated: 2024 - 11 - 20,
+        },
+        {
+            id: 3,
+            vote_title: 'ごはん食べたいですか',
+            added_user_id: 1,
+            is_Closed: false,
+            updated: 2024 - 11 - 20,
+        },
+    ];
+
     useEffect(() => {
         const fetchVotes = async () => {
             const res = await axios.get('http://localhost:8080/api/votes');
@@ -14,12 +38,14 @@ function App() {
     }, []);
 
     function optionList(votes) {
-        const votesComponents = votes.map((vote) => {
-            return <p>vote</p>;
+        return votes.map((vote) => {
+            return <li>{vote.vote_title}</li>;
         });
     }
+    const votesComponents = optionList(votes_test);
+    const testList = <ol>{votesComponents}</ol>;
 
-    return <></>;
+    return <>{testList}</>;
 }
 
 export default App;
