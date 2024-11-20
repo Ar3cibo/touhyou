d8e5d2bfa2ff83a526a8640988d7fcac3553fb61
Ar3cibo <knaito@protonmail.com>
2024-11-20 11:20:28 +0900

    [front] ページロード時のvotesデータ読み込み

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [votes, setVotes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const fetchVotes = async () => {
+            const res = await axios.get('http://localhost:8080/api/movies');
+            setVotes(res.data);
+            setLoading(false);
+        };
+    }, []);
 
-
-  return (
-    <>
-    </>
-  )
+    return <></>;
 }
 
-export default App
+export default App;
