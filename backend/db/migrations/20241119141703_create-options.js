f1e01f71b8a9a50843d909f57364e800c6356ba3
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-21 14:46:10 +0900

    修正：migrationファイルにforeign追加

--- backend/db/migrations/20241119141703_create-options.js
+++ backend/db/migrations/20241119141703_create-options.js
@@ -5,18 +5,14 @@
 const table="options"
 exports.up = async function(knex) {
     await knex.schema.createTable(table, (table) => {
-        table.integer("vote_title_id")
-            .references("vote_title.id")
-            .onDelete("CASCADE")
-            .notNullable();
+        table.integer("vote_title_id").notNullable();
         table.primary(["option_number", "vote_title_id"]);
         table.integer("option_number" ).notNullable();
         table.string("question").notNullable();
-        table.integer("user_id")
-            .references("user_table.id")
-            .onDelete("CASCADE")
-            .notNullable();
+        table.integer("user_id").notNullable();
         table.timestamp( "updated" ).notNullable();
+        table.foreign("vote_title_id").references("vote_title.id").onDelete("CASCADE");
+        table.foreign("user_id").references("user_table.id").onDelete("CASCADE")
     });
 };
 
