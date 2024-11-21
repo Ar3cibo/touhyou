fb83360b782417223779960d9ea31339a9525a2e
Ar3cibo <knaito@protonmail.com>
2024-11-21 09:19:21 +0900

    [front] コンポーネントを作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 // 依存関係のインストール
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useParams,
+} from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -7,59 +12,63 @@ import axios from 'axios';
 import VotesList from './components/VotesList.jsx';
 
 function App() {
-    const [votes, setVotes] = useState([]);
-    const [votesDetail, setVotesDetail] = useState([]);
-    const [loading, setLoading] = useState(true);
-
     const votes_test = [
         {
             id: 1,
             vote_title: '何県に行きたいですか',
             added_user_id: 1,
             is_Closed: false,
-            updated: 2024 - 11 - 20,
+            updated: '2024 - 11 - 20',
         },
         {
             id: 2,
             vote_title: 'どこの国に行きたいですか',
             added_user_id: 1,
             is_Closed: false,
-            updated: 2024 - 11 - 20,
+            updated: '2024 - 11 - 20',
         },
         {
             id: 3,
             vote_title: 'ごはん食べたいですか',
             added_user_id: 1,
             is_Closed: false,
-            updated: 2024 - 11 - 20,
+            updated: '2024 - 11 - 20',
         },
     ];
+    // const [votes, setVotes] = useState([]);
+    // const [votesDetail, setVotesDetail] = useState([]);
+    // const [loading, setLoading] = useState(true);
 
-    function VotesDetailsWrapper() {
-        const { id } = useParams();
-    }
-
-    async function fetchVotesDetail() {
-        const res = await axios.get('/api/votes:id');
-        setVotesDetail(res.data);
-        setLoading(false);
-    }
-
-    async function fetchVotes() {
-        const res = await axios.get('/api/votes');
-        setVotes(res.data);
-        setLoading(false);
-    }
-
-    function optionList() {}
-
-    useEffect(() => {
-        // fetchVotes();
-    }, []);
+    // function VotesDetailsWrapper() {
+    //     const { id } = useParams();
+    // }
+    //
+    // async function fetchVotesDetail() {
+    //     const res = await axios.get('/api/votes:id');
+    //     setVotesDetail(res.data);
+    //     setLoading(false);
+    // }
+    //
+    // async function fetchVotes() {
+    //     const res = await axios.get('/api/votes');
+    //     setVotes(res.data);
+    //     setLoading(false);
+    // }
+    //
+    // useEffect(() => {
+    //     // fetchVotes();
+    // }, []);
 
     return (
         <>
-            <VotesList votes={votes_test} />
+            <Router>
+                <Routes>
+                    <Route
+                        path={'/'}
+                        element={<VotesList votes={votes_test} />}
+                    />
+                </Routes>
+            </Router>
         </>
     );
 }
