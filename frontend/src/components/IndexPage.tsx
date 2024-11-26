58594887a18f3a7a62dc1c3e12f7ab4a641ef79c
natsuki_suzuki <natsuki_suzuki_aa@mail.toyota.co.jp>
2024-11-26 11:14:54 +0900

    IndexPage画面作成

--- frontend/src/components/IndexPage.tsx
+++ frontend/src/components/IndexPage.tsx
@@ -1,6 +1,71 @@
-import {Box, Center} from "@yamada-ui/react"
+import {Box, Center, Card, CardHeader, CardBody, Heading, Flex, Spacer, ListItem, List} from "@yamada-ui/react"
 
 export function IndexPage() {
+
+  //データ仮置き------------------------------------------------------------------------------------------
+
+  const questions = [
+    {id: 1, question: '好きなテーマパークは？',user_id: 1,is_closed:true,updated:'2024-11-01 01:01:01'},
+    {id: 2, question: '今一番いきたい場所は？',user_id: 1,is_closed:true,updated:'2024-11-02 01:01:01'},
+    {id: 3, question: '好きなお酒は？',user_id: 1,is_closed:true,updated:'2024-11-03 01:01:01'},
+    {id: 4, question: "好きな食べ物は？",user_id: 1,is_closed:true,updated:'2024-11-04 01:01:01'},
+    {id: 5, question: "住んでみたい国は？",user_id: 1,is_closed:true,updated:'2024-11-05 01:01:01'},
+  ]
+
+  const allOptions = [
+    {option_id: 1, question_id:1, option:'ディズニー',user_id:1,updated:'2024-11-01 01:01:01'},
+    {option_id: 1, question_id:2, option:'イタリア',user_id:1,updated:'2024-11-02 02:02:02'},
+    {option_id: 1, question_id:3, option:'ビール',user_id:2,updated:'2024-11-03 03:03:03'},
+    {option_id: 2, question_id:1, option:'ユニバ',user_id:2,updated:'2024-11-04 04:04:04'},
+    {option_id: 2, question_id:2, option:'北海道',user_id:3,updated:'2024-11-05 05:05:05'},
+    {option_id: 2, question_id:3, option:'ハイボール',user_id:3,updated:'2024-11-06 06:06:06'},
+  ]
+
+  const user_voting = [
+    {user_id: 1, question_id: 1,option_id:1},
+    {user_id: 2, question_id: 1,option_id:1},
+    {user_id: 3, question_id: 1,option_id:2},
+    {user_id: 1, question_id: 2,option_id:2},
+    {user_id: 2, question_id: 2,option_id:1},
+    {user_id: 3, question_id: 2,option_id:2},
+  ]
+
+  //-----------------------------------------------------------------------------------------------------
+
+  const questionCards = questions.map(question => {
+
+    const options = allOptions.filter(option => option.question_id === question.id);
+    const viewOptions = options.map(option => {
+      const votes = user_voting.filter(vote => {
+        return question.id === vote.question_id && option.option_id === vote.option_id
+      }).length
+
+      return (
+          <Flex>
+            <ListItem mr={'50px'}>{option.option}</ListItem>
+            <Spacer/>
+            <ListItem>投票数：{votes}</ListItem>
+          </Flex>
+      )
+    })
+
+
+
+    return(
+        <Card m={'12px'}>
+          <CardHeader>
+            <Heading size="md">{question.question}</Heading>
+          </CardHeader>
+          <CardBody>
+            <List>
+              {viewOptions}
+            </List>
+          </CardBody>
+        </Card>
+    )
+  })
+
+
   return (
     <>
       <Box p={'12px'} m={'12px'} rounded={'6px'} borderWidth={'1px'} borderColor={'black'} w={'960px'} ml={'auto'} mr={'auto'}>
@@ -12,7 +77,7 @@ export function IndexPage() {
             新規登録
           </Center>
         </Box>
-
+        {questionCards}
       </Box>
     </>
   )
