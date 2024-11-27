eea74a709aea648c0b20c48c342802de43003a4c
SUKE <yosuke.hioki@gmail.com>
2024-11-28 08:00:44 +0900

    デプロイのため index.js server.js backのpackage.json 修正

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
-// require("dotenv").config();
-// const { setupServer } = require("./server");
-// const express = require("express");
-// const app = setupServer();
-// app.use("/", express.static(__dirname + "/dist"))
-//
-// const environment = process.env.NODE_ENV
-// const PORT = process.env.PORT;
-// const VITE_URL = process.env.VITE_URL
-// console.log("PORT :",PORT);
-// app.listen(PORT, () => {
-//     if(environment === "development"){
-//         console.log("environment :", environment)
-//         console.log(`Expressサーバー起動中：http://localhost:${PORT}`);
-//     }else{
-//         console.log("environment :", environment)
-//         console.log(`Webサーバー起動中：${VITE_URL}:${PORT}`)
-//     }
-// });
+require("dotenv").config();
+const { setupServer } = require("./server");
+const express = require("express");
+const app = setupServer();
+app.use("/", express.static(__dirname + "/dist"))
+
+const environment = process.env.NODE_ENV
+const PORT = process.env.PORT;
+const VITE_URL = process.env.VITE_URL
+console.log("PORT :",PORT);
+app.listen(PORT, () => {
+    if(environment === "development"){
+        console.log("environment :", environment)
+        console.log(`Expressサーバー起動中：http://localhost:${PORT}`);
+    }else{
+        console.log("environment :", environment)
+        console.log("PORT :",PORT)
+        console.log(`Webサーバー起動中：${VITE_URL}`)
+    }
+});
--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ const routers=require("./routers")
 
 
 
-const app = express();
 function setupServer() {
+    const app = express();
     app.use(express.json());
     app.use(cors());
-    app.use("/", express.static(__dirname + "/dist"))
+
 
     passportSetting.authentication(app)
 
@@ -22,20 +22,3 @@ function setupServer() {
 }
 
 module.exports = { setupServer };
-
-
-
-require("dotenv").config();
-const environment = process.env.NODE_ENV
-const PORT = process.env.PORT;
-const VITE_URL = process.env.VITE_URL
-console.log("PORT :",PORT);
-app.listen(PORT, () => {
-    if(environment === "development"){
-        console.log("environment :", environment)
-        console.log(`Expressサーバー起動中：http://localhost:${PORT}`);
-    }else{
-        console.log("environment :", environment)
-        console.log(`Webサーバー起動中：${VITE_URL}:${PORT}`)
-    }
-});
